a38bdb3a8d12cb7c2353795e8b49906adccf8654
Laxman Chaudhary <chylaxman77@gmail.com>
2024-03-29 11:05:20 +0545

    new repo for hh

---
+++ code/PreproccesingAlatt.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+
+df = pd.read_csv("../data/halfHeusler.csv")
+df = df[[ 'ZX', 'ZAB', 'ZAB.1', 'mX', 'mAB', '∆mAB', 'rowX', 'rowAB', '∆rowAB',
+           'colX', 'colAB', '∆colAB', 'eX', 'eAB', '∆eAB', 'rX', 'rAB', '∆rAB',
+           'χX', 'χAB', '∆χAB', 'alatt', 'cv', 'cs', 'κ˜grain',
+           'κtransf', 'κforest', 'κanh','gap']]
+df = df.dropna()
+scaler = StandardScaler()
+df_scaled = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+X = df_scaled.drop(columns=['alatt'])
+y = df_scaled[['alatt']]
